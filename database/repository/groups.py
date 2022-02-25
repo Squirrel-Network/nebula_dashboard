e5331b75fb745e62a10a4679714f2d3e135189fb
BluLupo <hersel1991@gmail.com>
2022-02-25 23:01:45 +0100

    Add sb admin 2

--- database/repository/groups.py
+++ database/repository/groups.py
@@ -8,11 +8,11 @@ class GroupsRepository(Connection):
         query = Query.from_(groups).select("*").where(groups.id_group == '%s')
         q = query.get_sql(quote_char=None)
         return self._select(q, args)
-    
+
     def get_badwords_group(self, args=None):
         q = "SELECT * FROM groups_badwords WHERE tg_group_id = %s"
         return self._selectAll(q, args)
 
     def get_groups(self, args=None):
         q = "SELECT * FROM groups gr INNER JOIN nebula_dashboard nu ON gr.id_group = nu.tg_group_id WHERE nu.tg_id = %s"
-        return self._select(q, args)
+        return self._selectAll(q, args)

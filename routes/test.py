b27f0632b83d8b3ad8329819ec84f9bbdf7272a6
BluLupo <hersel1991@gmail.com>
2022-07-16 22:40:34 +0200

    fix and test ckeditor

--- routes/test.py
+++ routes/test.py
@@ -12,6 +12,6 @@ route_test = Blueprint('route_test', __name__)
 @route_test.route('/test', methods=['GET', 'POST'])
 @login_required
 def test():
-	a = request.form.get('ckeditortest')
+	a = request.form.get('valore_editor')
 	print(a)
 	return render_template("test.html")
